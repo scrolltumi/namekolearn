3367edf0962c83b4124fac2656b593e18840e6e2
geektumis <geektumis@gmail.com>
2020-02-18 01:24:47 +0200

    stuff and sleep

--- src/algos.py
+++ src/algos.py
@@ -5,6 +5,7 @@ import collections
 import unittest
 import huffman
 from collections import Counter
+from nameko.testing.services import worker_factory
 
 
 
@@ -21,5 +22,27 @@ class AlgosService:
     @rpc
     def huffmanEncode(self, stringList):
         return huffman.codebook(Counter(stringList).items())
-        
+
+    @rpc
+    def huffmanDecode(self, encodedString):
+        #Some code to decode
+        #Still to do. Not sure I understand
+        return encodedString
+    
+def test_algos_service():
+    # create worker with mock dependencies
+    service = worker_factory(AlgosService)
+
+    # test squareOddNumbers 
+    squared = service.squareOddNumbers([2, 7, 5, 64, 14])
+    assert squared == [2, 49, 25, 64, 14]
+
+    # test huffmanEncode 
+    encoded = service.huffmanEncode(['FR', 'UK', 'BE', 'IT', 'FR', 'IT', 'GR', 'FR', 'NL', 'BE', 'DE'])
+    assert encoded == {'FR': '10', 'UK': '011', 'BE': '110', 'IT': '111', 'GR': '010', 'NL': '001', 'DE': '000'}
+
+    
+
+    
+
     
